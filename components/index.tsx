35dbe5943d8c7c5565930110108c6f20f6892018
kaiesmahmud <kaiesmahmud@gmail.com>
2023-09-07 13:31:08 +0600

    new components added

--- components/index.tsx
+++ components/index.tsx
@@ -2,9 +2,12 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
 
 
 export {
     Hero,CustomButton,
-    Navbar, Footer
+    Navbar, Footer, 
+    SearchBar , CustomFilter
 }
